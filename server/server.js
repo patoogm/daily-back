e26c80e106518eb219549f288821cf486277d72d
Patricio García Millán <patogarciamillan@gmail.com>
2021-11-04 14:50:35 -0300

    Creacion de login

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 const createUser = require('../routes/users')
+const loginUser = require('../routes/login')
 
 app.use(express.json())
 
 //Ruta
 app.use('/', createUser)
+app.use('/', loginUser)
 
 app.listen(port, () => {
   console.log(`Estamos en el puerto ${port}`);
