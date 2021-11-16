f39ea7fdafaaae96e272515ae14314b463a65961
Teotihuacan <javier_389@hotmail.com>
2021-11-16 19:06:37 -0300

    express-validator in news

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,15 @@
 const { Router } = require('express')
 const route = Router()
 const { createNews, getNews, getNewsById, deleteNews, editNews} = require('../controllers/news')
+const { body } = require('express-validator')
 
 route.get('/get-news', getNews)
 route.get('/:newsId', getNewsById)
 
 route.post('/create-news',createNews)
+body('title').trim().escape().not().isEmpty().isLength({min:3,max:40}).withMessage('Invalid title'),
+body('bodyNews').trim().escape().not().isEmpty().isLength({min:10,max:400}).withMessage('Invalid article'),
+body('image').trim().escape().not().isEmpty().isLength({min:3,max:70}).withMessage('Invalid image'),
 route.delete('/news/:newsId', deleteNews)
 route.put('/news/:newsId', editNews)
 
